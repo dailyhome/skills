0cd54e96febd38b46a1a03c7b8435ead419cf9f3
s8sg <swarvanusg@gmail.com>
2018-06-08 10:56:57 +0900

    Initial commit with switch skill
    Signed-off-by: s8sg <swarvanusg@gmail.com>

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "Building Skills"
+
+export arch=$(uname -m)
+
+
+# Dummy : A dummy skill that represent similar behaviour as switch
+export eTAG="1.0.0"
+
+export dockerfile="Dockerfile.armhf"
+
+echo Building Skill s8sg/skill-switch-dummy:$eTAG
+if [ "$arch" != "armv7l" ]
+then
+    export dockerfile="Dockerfile"
+fi
+
+docker build -t s8sg/skill-switch-dummy:$eTAG -f ./switch-dummy/$dockerfile ./switch-dummy/ --no-cache
+
+
+
+
+export eTAG="1.0.0"
+
+export dockerfile="Dockerfile.armhf"
+
+echo Building Skill s8sg/skill-switch-rasp:$eTAG
+if [ "$arch" != "armv7l" ]
+then
+    echo need armhf platform to build the skill switch
+    exit 1
+fi
+
+docker build -t s8sg/skill-switch:$eTAG -f ./switch/$dockerfile ./switch/ --no-cache
---
+++ switch-dummy/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var state = map[string]bool{
+	"1": false,
+	"2": false,
+	"3": false,
+	"4": false,
+}
+
+func getValue(u *url.URL) string {
+	q := u.Query()
+	return q.Get("value")
+}
+
+func enable(w http.ResponseWriter, r *http.Request) {
+	value := getValue(r.URL)
+	state[value] = true
+	data, _ := json.Marshal(state)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func disable(w http.ResponseWriter, r *http.Request) {
+	value := getValue(r.URL)
+	state[value] = false
+	data, _ := json.Marshal(state)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func getstate(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(state)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func main() {
+	http.HandleFunc("/enable", enable)
+	http.HandleFunc("/disable", disable)
+	http.HandleFunc("/state", getstate)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
