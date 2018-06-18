3e77ac028fda28b6c399a6586c2c1d66c9413fdc
s8sg <swarvanusg@gmail.com>
2018-06-18 16:27:51 +0900

    build skill based on platform
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- build.sh
+++ build.sh
@@ -4,32 +4,15 @@ echo "Building Skills"
 
 export arch=$(uname -m)
 
-
-# Dummy : A dummy skill that represent similar behaviour as switch
-export eTAG="1.0.0"
+export eTAG="rasp"
 
 export dockerfile="Dockerfile.armhf"
 
 echo Building Skill s8sg/skill-switch-dummy:$eTAG
 if [ "$arch" != "armv7l" ]
 then
-    export dockerfile="Dockerfile"
+    export dockerfile="Dockerfile.dummy"
+    export $eTAG="dummy"
 fi
 
-docker build -t s8sg/skill-switch-dummy:$eTAG -f ./switch-dummy/$dockerfile ./switch-dummy/ --no-cache
-
-
-
-
-export eTAG="1.0.0"
-
-export dockerfile="Dockerfile.armhf"
-
-echo Building Skill s8sg/skill-switch-rasp:$eTAG
-if [ "$arch" != "armv7l" ]
-then
-    echo need armhf platform to build the skill switch
-    exit 1
-fi
-
-docker build -t s8sg/skill-switch:$eTAG -f ./switch/$dockerfile ./switch/ --no-cache
+docker build -t s8sg/skill-switch:$eTAG -f ./switch/$dockerfile ./switch/$eTag/ --no-cache
